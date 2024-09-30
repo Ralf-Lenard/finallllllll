e903d1f2e8c2ff5432c1c3f6b556c278fed37fb5
ralf <capinding151515@gmail.com>
2024-09-30 17:15:25 +0800

    addition process

--- calcu.java
+++ calcu.java
@@ -2,5 +2,13 @@ public class calcu{
     public static void(String[] args){
 
         System.out.print("hellloooooo");
+
+        int a = 5;
+        int b = 19;
+        int c = a + b;
+
+        System.out.print("hellloooooo worlddd" + c );
+
+
     }
 }
