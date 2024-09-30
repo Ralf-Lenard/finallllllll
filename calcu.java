821c12f0d88d9571e9e48cc7448a69bad8e2bfa5
ralf <capinding151515@gmail.com>
2024-09-30 17:29:18 +0800

    subtraction processs

--- calcu.java
+++ calcu.java
@@ -2,5 +2,15 @@ public class calcu{
     public static void(String[] args){
 
         System.out.print("hellloooooo");
+
+        int a = 5;
+        int b = 19;
+        int c = a + b;
+
+        System.out.println("hellloooooooooo worlddd" + c );
+
+        System.out.print("hellloooooooooo worlddd minussss" + c );
+
+
     }
 }
